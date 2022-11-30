38202f08c9aaa48c73ee63b1203d1fb53289d118
Adam Jeffreson <adam.jeffreson@gmail.com>
2022-11-30 14:43:11 +0000

    working on nav header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,26 +16,26 @@
         <!-- Scripts -->
         <script src="{{ asset('js/my.js') }}"></script>
     </head>
-{{--    <body class="font-sans antialiased">
-{{--        <div class="min-h-screen bg-gray-100">
+    <body class="font-sans antialiased">
+       <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
             <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
+
                 </div>
             </header>
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+
+                <div class="product-list">
+                    @yield('content')
+                </div>
             </main>
-        </div>--}}
-    <body>
-    <h1>Basic Example</h1>
-    <div class="product-list">
-        @yield('content')
-    </div>
+        </div>
+
+
     </body>
 </html>
