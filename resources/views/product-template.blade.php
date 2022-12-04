980cec79fff725dffe69c718caacea10da8a62aa
Adam Jeffreson <adam.jeffreson@gmail.com>
2022-12-04 11:49:34 +0000

    buttons working dynamically from template

--- resources/views/product-template.blade.php
+++ resources/views/product-template.blade.php
@@ -1,7 +1,7 @@
 @if(Route::currentRouteName()=='index')
     <div class="p-2 bg-blue-100">
 @else
-    <div class="p-8 max-w-xl bg-blue-100">
+    <div class="p-8 max-w-xl bg-green-100">
 @endif
     <div class ="bg-white p-1 rounded-lg shadow-lg">
         <h3 class = "text-blue-700 mb-4 text-lg font-bold">{{$product['artist']}}</h3>
@@ -10,7 +10,10 @@
             <p class = "text-gray-700">£{{$product['price']/100}}</p>
             @if(Route::currentRouteName()=='index')
                 <button value="{{$product['id']}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full select-product">Select</button>
+            @else
+                <button value="{{$product['id']}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full select-product">Buy</button>
             @endif
         </div>
     </div>
 </div>
+
