bdb56d9898828a4b429f1eee4b625c889b2e8ffa
Adam Jeffreson <adam.jeffreson@gmail.com>
2022-12-10 12:47:53 +0000

    fixed bug in product type selector

--- resources/views/products-filter.blade.php
+++ resources/views/products-filter.blade.php
@@ -1,5 +1,6 @@
-<div class="grid grid-cols-5">
-    @foreach($products as $product)
-        <x-product-card :product="$product" />
-    @endforeach
-</div>
+
+
+        @foreach($products as $product)
+            <x-product-card :product="$product" />
+        @endforeach
+
