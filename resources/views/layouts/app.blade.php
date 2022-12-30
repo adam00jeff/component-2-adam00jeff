65b21f0b55367dcb1371e49e85ef74edabd9b575
Adam Jeffreson <adam.jeffreson@gmail.com>
2022-12-30 11:25:20 +0000

    testing removal of dashboard.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
                         <div class="hidden border-2 p-1.5 sm:flex sm:items-center sm:ml-6 m-5">
                             <x-dropdown align="right" width="48">
                                 <x-slot name="trigger">
-                                    <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                    <button class="flex items-center text-m font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                                         <div>{{ Auth::user()->name }}</div>
 
                                         <div class="ml-1">
