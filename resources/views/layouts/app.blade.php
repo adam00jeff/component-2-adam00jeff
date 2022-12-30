02bd248d60516fc7fc47865748a8f3610105675c
Adam Jeffreson <adam.jeffreson@gmail.com>
2022-12-30 12:01:21 +0000

    removed dashboard and navigation blades
    styled the menu and naviation links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,86 +1,94 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-        <script src="{{ asset('js/my.js') }}"></script>
-    </head>
-    <body>
-       <div class="min-h-screen bg-gray-100">
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/my.js') }}"></script>
+</head>
+<body>
+<div class="min-h-screen bg-gray-100">
 
-            <!-- Page Heading -->
-            <header class="flex justify-between border-bottom-double border-8 bg-white shadow">
-                <div class="flex flex-col items-start border-2 rounded-lg m-5">
-                    <a href="{{route('home')}}"><img src="{{asset('images/om.jpg')}}" alt="logo" class="m-5"></a>
-                    <h2 class = "font-bold text-lg self-center"> Component 2</h2>
+    <!-- Page Heading -->
+    <header class="flex justify-between border-bottom-double border-8 bg-white shadow">
+        <!-- app logo -->
+        <div class="flex flex-col items-start border-2 rounded-lg m-5">
+            <a href="{{route('home')}}"><img src="{{asset('images/om.jpg')}}" alt="logo" class="m-5"></a>
+            <h2 class="font-bold text-lg self-center"> Component 2</h2>
 
+        </div>
+        <!--    login/logout dropdown & register/login link-->
+        <div class="p-4">
+            <!-- checks for a logged-in user
+            shows login/register if not-->
+            @auth
+                <div class="hidden border-2  sm:flex sm:items-center sm:ml-6 m-5">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button
+                                class="flex text-2xl items-center rounded-sm shadow-2xl p-2 font-bold bg-yellow-300 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                <div>{{ Auth::user()->name }}</div>
+
+                                <div class="ml-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                         viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd"
+                                              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                              clip-rule="evenodd"/>
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <!-- Authentication -->
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <x-dropdown-link :href="route('logout')"
+                                                 onclick="event.preventDefault();
+                                                this.closest('form').submit();"  class="text-medium font-extrabold hover:bg-yellow-300">
+                                    {{ __('Log Out') }}
+                                </x-dropdown-link>
+                            </form>
+                        </x-slot>
+                    </x-dropdown>
                 </div>
-                <div class="p-4">
-                    @auth
-                        <div class="hidden border-2 p-1.5 sm:flex sm:items-center sm:ml-6 m-5">
-                            <x-dropdown align="right" width="48">
-                                <x-slot name="trigger">
-                                    <button class="flex items-center text-m font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                                        <div>{{ Auth::user()->name }}</div>
+            @else
+                <!-- shown when no user is logged in-->
+                <a href="{{ route('login') }}" class="flex-rows rounded-xl m-1 shadow-2xl rounded-sm bg-yellow-300 border-blue-300 p-3 font-semibold text-gray-600 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">Login</a>
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="flex-rows rounded-xl m-1 shadow-2xl rounded-sm bg-yellow-300 border-blue-300 p-3 font-semibold text-gray-600 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">Register</a>
+                @endif
+            @endif
+        </div>
 
-                                        <div class="ml-1">
-                                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                            </svg>
-                                        </div>
-                                    </button>
-                                </x-slot>
+    </header>
+    @include('layouts.menu')
 
-                                <x-slot name="content">
-                                    <!-- Authentication -->
-                                    <form method="POST" action="{{ route('logout') }}">
-                                        @csrf
+    <page-header class="flex justify-center font-bold text-lg self-center p-3">
 
-                                        <x-dropdown-link :href="route('logout')"
-                                                         onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                            {{ __('Log Out') }}
-                                        </x-dropdown-link>
-                                    </form>
-                                </x-slot>
-                            </x-dropdown>
-                        </div>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        @endif
-                    @endif
-                </div>
-
-            </header>
-           @include('layouts.menu')
-
-           <page-header class="flex justify-center font-bold text-lg self-center p-3">
-
-           {{$header ?? ''}}
-           </page-header>
-            <!-- Page Content -->
-            <main class="justify-center mx-auto border-bottom-double border-2 self-center p-3">
-            {{$slot ?? ''}}
+        {{$header ?? ''}}
+    </page-header>
+    <!-- Page Content -->
+    <main class="justify-center mx-auto border-bottom-double border-2 self-center p-3">
+        {{$slot ?? ''}}
 
 
-
-            </main>
+    </main>
 
 
 </div>
-    </body>
+</body>
 </html>
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,27 +1,27 @@
 <nav class = "flex justify-center">
-    <div class = "flex m-5 shadow-2xl rounded-sm bg-yellow-300 border-blue-300">
-        <a href="{{route('home')}}" class="text-gray-700 p-5 font-semibold">
+    <div class = "flex rounded-xl m-5 shadow-2xl rounded-sm bg-yellow-300 border-blue-300">
+        <a href="{{route('home')}}" class=" p-5 font-semibold text-gray-600 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
         Home
         </a>
     </div>
-    <div class = "flex m-5 shadow-2xl rounded-sm bg-yellow-300 border-blue-300">
-        <a href="{{route('index')}}" class="text-gray-700 p-5 font-semibold">
+    <div class = "flex rounded-xl m-5 shadow-2xl rounded-sm bg-yellow-300 border-blue-300">
+        <a href="{{route('index')}}" class="p-5 font-semibold text-gray-600 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
             Products
         </a>
     </div>
 
 
     @can('edit-product')
-    <div class = "flex m-5 shadow-2xl rounded-sm bg-yellow-300 border-blue-300">
-        <a href="{{route('create')}}" class="text-gray-700 p-5 font-semibold">
+    <div class = "flex rounded-xl m-5 shadow-2xl rounded-sm bg-yellow-300 border-blue-300">
+        <a href="{{route('create')}}" class="p-5 font-semibold text-gray-600 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
             Add Product
         </a>
     </div>
 
     @endcan
 
-    <div class = "flex m-5 shadow-2xl rounded-sm bg-yellow-300 border-blue-300">
-<label for="producttype" class="m-4">Search:</label>
+    <div class = "flex rounded-xl m-5 shadow-2xl rounded-sm bg-yellow-300 border-blue-300">
+<label for="producttype" class="font-semibold p-1 m-4 text-gray-600 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">Search:</label>
 <input class="flex w-full h-small bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 m-1 search"
                            id="search" name="Search" type="text" placeholder="Search">
 </div>
