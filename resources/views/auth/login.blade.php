7644c659d1d1d8845940b42a35ed19546070d47c
Adam Jeffreson <adam.jeffreson@gmail.com>
2023-01-01 15:40:07 +0000

    login page would trap unregistered users, added register link

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,11 +43,13 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-{{--                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif--}}
+                <a href="{{ route('register') }}" class="underline text-sm text-gray-600 hover:text-gray-900">Register</a>
+
+                {{--                @if (Route::has('password.request'))
+                                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                                        {{ __('Forgot your password?') }}
+                                    </a>
+                                @endif--}}
 
                 <x-button class="ml-3">
                     {{ __('Log in') }}
